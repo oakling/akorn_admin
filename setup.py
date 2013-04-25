4ce46f9b7b63ef392f693a74e6115b9d584dda7d
Duncan Mortimer <dmorti@gmail.com>
2013-04-25 20:03:59 +0100

    package as an egg

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join("akorn", "admin", "version.txt")).read().strip()
+
+setup(name='akorn.admin',
+      version=version,
+      description="",
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Akorn',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['akorn'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
